b6dddd49dc8740b5010b5bc9f90687186ef7ba26
Nishant Gerald <nishant.gerald@gmail.com>
2024-05-03 14:22:59 -0400

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import setup
 
 setup(
     name='apparata',
-    version='0.1.1',
+    version='0.1.2',
+    url='https://github.com/nishantgerald/apparata',
+    license='MIT',
     description='A collection of useful functions for generating random data.',
     author='Nishant Gerald',
     author_email='nishantgerald@gmail.com',
