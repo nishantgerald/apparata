98d85540a55fdd7fc7c735637fb3391cc877e0a9
Nishant Gerald <nishant.gerald@gmail.com>
2024-05-03 12:39:25 -0400

    created setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='apparata',
+    version='0.1.0',
+    description='A collection of useful functions for generating random data.',
+    author='Nishant Gerald',
+    author_email='nishantgerald@gmail.com',
+    packages=['apparata'],
+    install_requires=['faker', 'pandas'],
+)
